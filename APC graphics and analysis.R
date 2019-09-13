25f20183de8ec1cd692b3f1041e5d6e074dc84cb
BenBarnes <barnes.benjamin@gmail.com>
2019-09-13 23:52:21 +0200

    Add glm example, include lung cancer among men

--- APC graphics and analysis.R
+++ APC graphics and analysis.R
@@ -4,14 +4,16 @@ base <- "/Users/benbarnes2/Documents/DGEpi/Modelling Workshop/Workshop APC/R cod
 if(!exists("finBladPop")) {
   source(file.path(base, "APC data prep.R"))
 }
+p0 <- 1976
+c0 <- 1906
 
 ## Plot rates by calendar year and period, group by age group
 ## Bladder cancer
-ggplot(finBladMenAPC[meanAge >= 50 & perBeg > 1955], aes(x = cohMid, y = newCrudeRate,
+ggplot(finBladMenAPC[meanAge >= 30 & perBeg > 1955], aes(x = cohMid, y = newCrudeRate,
   color = ageGroup)) +
   geom_line() + geom_point() + coord_trans(y = "log2")
 
-ggplot(finBladMenAPC[meanAge >= 50 & perBeg > 1955], aes(x = perBeg, y = newCrudeRate,
+ggplot(finBladMenAPC[meanAge >= 30 & perBeg > 1955], aes(x = perBeg, y = newCrudeRate,
   color = ageGroup)) +
   geom_line() + geom_point() + coord_trans(y = "log2")
 
@@ -21,6 +23,15 @@ ggplot(finLungWomenAPC[meanAge >= 50 & perBeg > 1955], aes(x = cohMid, y = newCr
   geom_line() + geom_point() + coord_trans(y = "log2")
 
 ggplot(finLungWomenAPC[meanAge >= 50 & perBeg > 1955], aes(x = perBeg, y = newCrudeRate,
+  color = ageGroup)) +
+  geom_line() + geom_point() + coord_trans(y = "log2")
+
+## Lung cancer among men
+ggplot(finLungMenAPC[meanAge >= 50 & perBeg > 1955], aes(x = cohMid, y = newCrudeRate,
+  color = ageGroup)) +
+  geom_line() + geom_point() + coord_trans(y = "log2")
+
+ggplot(finLungMenAPC[meanAge >= 50 & perBeg > 1955], aes(x = perBeg, y = newCrudeRate,
   color = ageGroup)) +
   geom_line() + geom_point() + coord_trans(y = "log2")
 
@@ -41,16 +52,43 @@ ggplot(finBladMenAPC[meanAge >= 30 & meanAge < 80], aes(x = cohMid, y = predInc,
   geom_line() + geom_point() + coord_trans(y = "log2")
 
 ## Age-period model using splines and 1-year periods
+## glm function
+agePerGLM <- glm(D ~ splines::bs(A, df = 8) + splines::bs(P - p0, df = 8) +
+  offset(log(Y)),
+  data = finBladPopMen, family = poisson(link = "log"))
+summary(agePerGLM)
 
+finBladPopMen[, predAPglm := predict(agePerGLM,
+  newdata = finBladPopMen, type = "response")]
+
+ggplot(finBladPopMen[meanAge >= 30 & meanAge < 80], aes(x = P, y = predAPglm,
+  color = ageGroup)) +
+  geom_line() + geom_point() + coord_trans(y = "log2")
+
+ggplot(finBladPopMen[meanAge >= 30 & meanAge < 80], aes(x = cohMid, y = predAPglm,
+  color = ageGroup)) +
+  geom_line() + geom_point() + coord_trans(y = "log2")
+
+## apc.fit function
 agePeriod <- apc.fit(finBladPopMen[meanAge >= 30 & meanAge < 80], dist = "poisson",
   model = "bs", npar = c(A = 9, P = 9, C = 8), parm = "AP-C",
-  scale = 1e5, ref.p = 1982.5)
+  scale = 1e5, ref.p = p0)
 
 ## This is the age-period model component:
 summary(agePeriod[["Model"]][[1]])
 
 ## Cohort effect estimated based on residuals from age-period model
 plot(agePeriod, lty = c(1, 1, 3), col = "red", shade = TRUE)
+
+finBladPopMen[meanAge >= 30 & meanAge < 80,
+  predAPepi := predict(agePeriod[["Model"]][[1]], type = "response")]
+ggplot(finBladPopMen[meanAge >= 30 & meanAge < 80], aes(x = P, y = predAPepi,
+  color = ageGroup)) +
+  geom_line() + geom_point() + coord_trans(y = "log2")
+
+ggplot(finBladPopMen[meanAge >= 30 & meanAge < 80], aes(x = cohMid, y = predAPepi,
+  color = ageGroup)) +
+  geom_line() + geom_point() + coord_trans(y = "log2")
 
 ## Age-Cohort model
 ageCohort <- apc.fit(finBladPopMen[meanAge >= 30 & meanAge < 80], dist = "poisson",
